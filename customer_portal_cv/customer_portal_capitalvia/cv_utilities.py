62ae70da85d078424a990cf7f4dd06de1fe9a62b
hari <harikishorsingh060@gmail.com>
2021-04-05 16:35:54 +0530

    Mark Recommendation read

--- customer_portal_cv/customer_portal_capitalvia/cv_utilities.py
+++ customer_portal_cv/customer_portal_capitalvia/cv_utilities.py
@@ -1026,3 +1026,12 @@ def mark_executed():
     frappe.db.set_value("Signal Logs", signal_log, "executed", 1)
     frappe.db.commit()
     return "SUCCESS"
+
+@frappe.whitelist()
+def mark_recommendation();
+    user = check_permissions()
+    signal_log = frappe.form_dict.get('signal_log')
+    if not signal_log:
+        frappe.throw("Signal log")
+
+    frappe.db.set_value("Signal Logs", signal_log, "mark_recm", 1)
